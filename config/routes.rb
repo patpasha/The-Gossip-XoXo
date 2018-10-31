ccb89be48849b0d6a3a1389fb0c3eeae321101d1
Patrick Raza <43625513+patpasha@users.noreply.github.com>
2018-10-31 01:54:56 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get '/', to: 'static#home'
-  get 'static/home', to: 'static#home'
-  get 'static/welcome', to: 'static#welcome'
-  post 'static/welcome', to: 'static#welcome'
-  get 'static/team', to: 'static#team'
-  get 'static/contact',to: 'static#contact'
+  get '/home', to: 'static#home'
+  get '/welcome', to: 'static#welcome'
+  post '/welcome', to: 'static#welcome'
+  get '/team', to: 'static#team'
+  get '/contact',to: 'static#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
